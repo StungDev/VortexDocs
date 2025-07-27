29fb6a11a77fb3309e2dc077e1ea8c865631118e
StungDev <103583641+StungDev@users.noreply.github.com>
2025-07-27 20:30:57 +0100

    upd3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://StungDev.github.io',
+  url: 'https://stungdev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/VortexDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'StungDev', // Usually your GitHub org/user name.
-  projectName: 'VortexDocs', // Usually your repo name.
+  organizationName: 'stungdev', // Usually your GitHub org/user name.
+  projectName: 'vortexdocs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
